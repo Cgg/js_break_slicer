8d8f775c9058798036e9240c8bf775796711674a
Cgg <clement.geiger@gmail.com>
2014-07-26 15:26:09 +0200

    math: add a shuffle function.
    Everyday I'm shuffling.

--- math.js
+++ math.js
@@ -81,3 +81,27 @@ function variance(values, start, length, mean) {
   return V;
 }
 
+/**
+ * @brief shuffle take an array and shuffle it in place using Fisher-Yates
+ * algorithm.
+ * @param array the array to shuffle.
+ * @return the shuffled array, which is the same as the array passed as param.
+ */
+function shuffle(array) {
+  var result = array.slice(0);
+  var m = array.length, i;
+  var swapBuf;
+
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    swapBuf = array[m];
+    array[m] = array[i];
+    array[i] = swapBuf;
+  }
+
+  return array;
+}
